6693b6def520b3e706c45ddf2b083c58bcdd8bac
Tommaso Rosi <tommasorosi@gmail.com>
2024-11-03 22:16:17 +0100

    Refactor and document subtract()
    Rewrites subtract() using simpler and more generic argument names and
    introducing minimal type checking. Also adds documentation for the
    function.

--- R/multiply.R
+++ R/multiply.R
@@ -3,9 +3,22 @@ add <- function(firstvariablename = "7", secondvariablename) {
   firstvariablename + secondvariablename + thirdvariablename
 }
 
-# export-me
-subtract <<- function(zzz2, zzz1) {
-  zzz1 - zzz2
+#' Algebraic Subtraction
+#'
+#' @description Finds the algebraic difference between two numeric vectors.
+#'
+#' @param x A numeric vector from which \code{y} will be subtracted.
+#' @param y A numeric vector to subtract.
+#'
+#' @return A numeric vector with the difference between \code{x} and \code{y}.
+#' @export
+#'
+#' @examples
+#' subtract(c(10, 10), c(7, 20))
+#' > [1]  3 -10
+subtract <- function(x, y) {
+  stopifnot(is.numeric(x), is.numeric(y))
+  x - y
 }
 
 print.safespace <- function(x, ...) {
