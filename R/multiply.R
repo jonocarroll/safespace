jonocarroll/safespace

commit 6693b6def520b3e706c45ddf2b083c58bcdd8bac

Refactor and document subtract()
Rewrites subtract() using simpler and more generic argument names and
introducing minimal type checking. Also adds documentation for the
function.

R/multiply.R
@@ -3,9 +3,22 @@ add <- function(firstvariablename = "7", secondvariablename) {
   firstvariablename + secondvariablename + thirdvariablename
 }
 
-# export-me
-subtract <<- function(zzz2, zzz1) {
-  zzz1 - zzz2
+#' Algebraic Subtraction
+#'
+#' @description Finds the algebraic difference between two numeric vectors.
+#'
+#' @param x A numeric vector from which \code{y} will be subtracted.
+#' @param y A numeric vector to subtract.
+#'
+#' @return A numeric vector with the difference between \code{x} and \code{y}.
+#' @export
+#'
+#' @examples
+#' subtract(c(10, 10), c(7, 20))
+#' > [1]  3 -10
+subtract <- function(x, y) {
+  stopifnot(is.numeric(x), is.numeric(y))
+  x - y
 }
 
 print.safespace <- function(x, ...) {

man/subtract.Rd
@@ -1,0 +1,23 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/multiply.R
+\name{subtract}
+\alias{subtract}
+\title{Algebraic Subtraction}
+\usage{
+subtract(x, y)
+}
+\arguments{
+\item{x}{A numeric vector from which \code{y} will be subtracted.}
+
+\item{y}{A numeric vector to subtract.}
+}
+\value{
+A numeric vector with the difference between \code{x} and \code{y}.
+}
+\description{
+Finds the algebraic difference between two numeric vectors.
+}
+\examples{
+subtract(c(10, 10), c(7, 20))
+> [1]  3 -10
+}

DESCRIPTION
@@ -15,3 +15,4 @@ Encoding: UTF-8
 LazyData: true
 URL: https://github.com/jonocarroll/safespace
 BugReports: https://github.com/jonocarroll/safespace/issues
+RoxygenNote: 7.3.2

commit e7c42a4091890a2edc00c88eff0855eeef65b31b

Fix @example tag in subtract() documentation
Removes a line incorrectly introduced in commit 6693b6d: @examples tags
should only contain statements that can be run without errors as part of
RMD CMD checks.

R/multiply.R
@@ -15,7 +15,6 @@ add <- function(firstvariablename = "7", secondvariablename) {
 #'
 #' @examples
 #' subtract(c(10, 10), c(7, 20))
-#' > [1]  3 -10
 subtract <- function(x, y) {
   stopifnot(is.numeric(x), is.numeric(y))
   x - y

man/subtract.Rd
@@ -19,5 +19,4 @@ Finds the algebraic difference between two numeric vectors.
 }
 \examples{
 subtract(c(10, 10), c(7, 20))
-> [1]  3 -10
 }