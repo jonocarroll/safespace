d18167fab70bf556dc1fd8cd5afb0d4e8fb0e3e1
Jonathan Carroll <github@jcarroll.com.au>
2025-10-08 09:41:02 +1030

    breakage

--- R/multiply.R
+++ R/multiply.R
@@ -3,12 +3,11 @@ add <- function(firstvariablename = "7", secondvariablename) {
   firstvariablename + secondvariablename + thirdvariablename
 }
 
-#' Algebraic Subtraction
+#' Algebraic Multiplication
 #'
 #' @description Finds the algebraic difference between two numeric vectors.
 #'
 #' @param x A numeric vector from which \code{y} will be subtracted.
-#' @param y A numeric vector to subtract.
 #'
 #' @return A numeric vector with the difference between \code{x} and \code{y}.
 #' @export
@@ -17,12 +16,13 @@ add <- function(firstvariablename = "7", secondvariablename) {
 #' subtract(c(10, 10), c(7, 20))
 subtract <- function(x, y) {
   stopifnot(is.numeric(x), is.numeric(y))
-  x - y
+  y - x
 }
 
 print.safespace <- function(x, ...) {
   # thisi s the print method for the safespace package
-  format(x, ...)
+  formats(x, ...)
 }
 
 
+exit
